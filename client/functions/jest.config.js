46f64634e54d708df1295417a0dfd5efc1b3081b
Akash <akash_tharuka@yahoo.com>
2022-11-19 05:13:16 +0530

    firebase hosting client

--- client/functions/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    testRegex: 'src(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-    testPathIgnorePatterns: ['lib/', 'node_modules/'],
-    moduleFileExtensions: ['js','ts','tsx','jsx','json','node'],
-    testEnvironment: 'node',
-    rootDir: 'src'
-}
