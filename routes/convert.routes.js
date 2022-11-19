889c623b849dcbe6e9a814f9499e61da84f7bd94
Akash <akash_tharuka@yahoo.com>
2022-11-19 17:38:25 +0530

    merge conflicts resolved

--- routes/convert.routes.js
+++ routes/convert.routes.js
@@ -28,7 +28,7 @@ let uploadDocument = multer({
     storage: getStorage(DIR),
     fileFilter: (req, file, cb) => {
         // Allowed extensions
-        const fileTypes = /.docx|.doc|.xlsx|.xls|.pptx|.ppt/;
+        const fileTypes = /.docx|.doc|.xlsx|.xls|.pptx|.ppt|.png|.jpeg|.webp|.pdf/;
         // Check extension
         const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
 		console.log(path.extname(file.originalname).toLowerCase(), extName);
@@ -43,106 +43,19 @@ let uploadDocument = multer({
     }
 });
 
-let uploadXlsx = multer({
-    storage: getStorage(DIR),
-    fileFilter: (req, file, cb) => {
-        // Allowed extensions
-        const fileTypes = /xlsx/;
-        // Check extension
-        const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+router.post('/docxToPdfUpload', uploadDocument.single("docx"), convertController.handleDocxToPdf);
+router.post('/docToPdfUpload', uploadDocument.single("doc"), convertController.handleDocToPdf);
+router.post('/xlsxToPdfUpload', uploadDocument.single("xlsx"), convertController.handleXlsxToPdf);
+router.post('/xlsToPdfUpload', uploadDocument.single("xls"), convertController.handleXlsToPdf);
+router.post('/pptxToPdfUpload', uploadDocument.single("pptx"), convertController.handlePptxToPdf);
+router.post('/pptToPdfUpload', uploadDocument.single("ppt"), convertController.handlePptToPdf);
 
-        if (file.mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' && extName){
-            cb(null, true);
-        }
-        else{
-            cb(null, false);
-            return cb(new Error('Only .xlsx format is allowed'));
-        }
-    }
-});
+router.post('/pngToPdfUpload', uploadDocument.single("png"), convertController.handlePngToPdf);
+router.post('/jpgToPdfUpload', uploadDocument.single("jpg"), convertController.handleJpgToPdf);
 
-let uploadPptx = multer({
-	storage: getStorage(DIR),
-	fileFilter: (req, file, cb) => {
-        // Allowed extensions
-        const fileTypes = /pptx/;
-        // Check extension
-        const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-
-        if (file.mimetype === 'application/vnd.openxmlformats-officedocument.presentationml.presentation' && extName){
-            cb(null, true);
-        }
-        else{
-            cb(null, false);
-            return cb(new Error('Only .pptx format is allowed'));
-        }
-    }
-});
-
-let uploadPng = multer({
-	storage: getStorage(DIR),
-	fileFilter: (req, file, cb) => {
-		console.log(file);
-        // Allowed extensions
-        const fileTypes = /png/;
-        // Check extension
-        const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-
-        if (file.mimetype === 'image/png' && extName){
-            cb(null, true);
-        }
-        else{
-            cb(null, false);
-            return cb(new Error('Only .png format is allowed'));
-        }
-    }
-});
-
-let uploadJpg = multer({
-	storage: getStorage(DIR),
-	fileFilter: (req, file, cb) => {
-        // Allowed extensions
-        const fileTypes = /jpg/;
-        // Check extension
-        const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-
-        if (file.mimetype === 'image/jpeg' && extName){
-            cb(null, true);
-        }
-        else{
-            cb(null, false);
-            return cb(new Error('Only .jpg format is allowed'));
-        }
-    }
-});
-
-let uploadPdf = multer({
-	storage: getStorage(DIR),
-	fileFilter: (req, file, cb) => {
-        // Allowed extensions
-        const fileTypes = /pdf/;
-        // Check extension
-        const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
-
-        if (file.mimetype === 'application/pdf' && extName){
-            cb(null, true);
-        }
-        else{
-            cb(null, false);
-            return cb(new Error('Only .pdf format is allowed'));
-        }
-    }
-});
-
-// router.post('/docxToPdfUpload', uploadDocx.single("docx"), convertController.handleDocxToPdf);
-// router.post('/xlsxToPdfUpload', uploadXlsx.single("xlsx"), convertController.handleXlsxToPdf);
-// router.post('/pptxToPdfUpload', uploadPptx.single("pptx"), convertController.handlePptxToPdf);
-// router.post('/pngToPdfUpload', uploadPng.single("png"), convertController.handlePngToPdf);
-
-// router.post('/pdfToJpgUpload', uploadPdf.single("pdf"), convertController.handlePdfToJpg);
-// router.post('/pdfToPngUpload', uploadPdf.single("pdf"), convertController.handlePdfToPng);
-// router.post('/pdfToWebpUpload', uploadPdf.single("pdf"), convertController.handlePdfToWebp);
-// router.post('/jpgToPdfUpload', uploadJpg.single("jpg"), convertController.handlePngToPdf);
+router.post('/pdfToJpgUpload', uploadDocument.single("pdf"), convertController.handlePdfToJpg);
+router.post('/pdfToPngUpload', uploadDocument.single("pdf"), convertController.handlePdfToPng);
+router.post('/pdfToWebpUpload', uploadDocument.single("pdf"), convertController.handlePdfToWebp);
 
 router.post('/convertDocument', uploadDocument.single("file"), convertController.handleDocumentConvertor);
 
